d40b38941bb21ae4a6baacdb3fb527e52755bd6b
明亮 <renmingliang5@crc.com.hk>
2020-07-20 14:51:26 +0800

    调整react相对资源路径

---
+++ micro/react-sub3/precache-manifest.8a9f147f90d9ba04e4491858c4d72a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839737398dd406eb09430ce131c7d1d5",
+    "url": "/micro/react-sub3/index.html"
+  },
+  {
+    "revision": "fc225838b4123ea8a602",
+    "url": "/micro/react-sub3/static/css/main.4d3e809c.chunk.css"
+  },
+  {
+    "revision": "31acdd4d69811f16af7d",
+    "url": "/micro/react-sub3/static/js/2.4347c1d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/micro/react-sub3/static/js/2.4347c1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc225838b4123ea8a602",
+    "url": "/micro/react-sub3/static/js/main.651750fa.chunk.js"
+  },
+  {
+    "revision": "800b920f8503122d30c7",
+    "url": "/micro/react-sub3/static/js/runtime-main.3b1038a1.js"
+  }
+]);
--- micro/react-sub3/service-worker.js
+++ micro/react-sub3/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3505e0203815914c9867c5379ab63928.js"
+  "/micro/react-sub3/precache-manifest.8a9f147f90d9ba04e4491858c4d72a7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/micro/react-sub3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
